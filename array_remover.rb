6c9f4c6e59289c0fef76172f53c79c53b937a240
Darin Haener <dphaener@gmail.com>
2013-09-11 15:51:29 -0700

    A class that takes an array of characters and uses the remove_j method to remove all j's and return a new array.

---
+++ array_remover.rb
@@ -0,0 +1,18 @@
+class CharRemove
+  attr_accessor :char_array
+  
+  def initialize(character_array)
+    @char_array = character_array
+  end
+  
+  def remove_j
+    new_array = []
+    @char_array.each do |c|
+      if c == "j"
+      else
+        new_array.push(c)
+      end
+    end
+    new_array
+  end
+end
